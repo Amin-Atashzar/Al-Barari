8787ce37422d131064dd65b3096bf21f3b082d55
Amin-Atashzar <49231593+Amin-Atashzar@users.noreply.github.com>
2019-04-20 14:10:59 +0430

    'start'

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#btnPhone').click(function(){
+		$('#phone').slideToggle();
+	})				
+	
+	$('#btnEmail').click(function(){
+		$('#email').slideToggle();
+	})				
+	
+	$('#btnOffer').click(function(){
+		$('#offer').slideToggle();
+	})
+})
+	
